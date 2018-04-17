0041764073c305d709ccf51359bdca3d98fe00a5
Leigh-ann <leighann.gant@gmail.com>
2018-04-17 11:32:45 +0100

    code added to grid to map 2d array (grid) and check for true status - adds adds box componement for each area in grid (box to be created)

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,12 @@ import Grid from './Grid';
 class Main extends React.Component {
   constructor(){
     super()
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
     this.state = {
       generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
     }
   }
   render() {
@@ -13,6 +17,10 @@ class Main extends React.Component {
       <div>
         <h1> The Game of Life </h1>
         <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
         />
         <h2> Generations: {this.state.generation} </h2>
       </div>
